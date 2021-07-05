0fbb6fc48eddb83c32cd2098104661ada6396d95
bruno cuconato (@odanoburu) <bcclaro@gmail.com>
2021-07-05 09:11:55 -0300

    * working q2
      verified to return same results as original query in TPC-H
      qualification DB

--- queries/q-02.sql
+++ queries/q-02.sql
@@ -13,11 +13,11 @@ from part,
      region
 where p_partkey = ps_partkey
   and s_suppkey = ps_suppkey
-  and p_size = [SIZE]
-  and p_type like '%[TYPE]'
+  and p_size = 15 -- QBD
+  and p_type like '%BRASS' -- QDB
   and s_nationkey = n_nationkey
   and n_regionkey = r_regionkey
-  and r_name = '[REGION]'
+  and r_name = 'EUROPE' -- QDB
   and ps_supplycost =
     (select min(ps_supplycost)
      from partsupp,
@@ -28,8 +28,22 @@ where p_partkey = ps_partkey
        and s_suppkey = ps_suppkey
        and s_nationkey = n_nationkey
        and n_regionkey = r_regionkey
-       and r_name = '[REGION]' )
+       and r_name = 'EUROPE' ) -- QDB
 order by s_acctbal desc,
          n_name,
          s_name,
          p_partkey;
+
+-- (sqlite)
+-- QUERY PLAN
+-- |--SCAN TABLE partsupp
+-- |--SEARCH TABLE supplier USING INTEGER PRIMARY KEY (rowid=?)
+-- |--SEARCH TABLE part USING INTEGER PRIMARY KEY (rowid=?)
+-- |--CORRELATED SCALAR SUBQUERY 1
+-- |  |--SEARCH TABLE partsupp USING INDEX sqlite_autoindex_PARTSUPP_1 (PS_PARTKEY=?)
+-- |  |--SEARCH TABLE supplier USING INTEGER PRIMARY KEY (rowid=?)
+-- |  |--SEARCH TABLE nation USING INTEGER PRIMARY KEY (rowid=?)
+-- |  `--SEARCH TABLE region USING INTEGER PRIMARY KEY (rowid=?)
+-- |--SEARCH TABLE nation USING INTEGER PRIMARY KEY (rowid=?)
+-- |--SEARCH TABLE region USING INTEGER PRIMARY KEY (rowid=?)
+-- `--USE TEMP B-TREE FOR ORDER BY
