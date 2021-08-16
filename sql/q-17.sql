c12bf3ab30af6a64a50c19cf75918fb3fe7f262d
bruno cuconato (@odanoburu) <bcclaro@gmail.com>
2021-08-16 12:09:46 -0300

    * add Q17
      [TBV] to be verified

---
+++ sql/q-17.sql
@@ -0,0 +1,10 @@
+select sum(l_extendedprice) / 7.0 as avg_yearly
+from lineitem,
+     part
+where p_partkey = l_partkey
+  and p_brand = 'Brand#23' -- QDB
+  and p_container = 'MED BOX' -- QDB
+  and l_quantity <
+    (select 0.2 * avg(l_quantity)
+     from lineitem
+     where l_partkey = p_partkey );
