3b914f53c3d7e20c72b4408debebafc30ec47576
bruno cuconato (@odanoburu) <bcclaro@gmail.com>
2021-09-28 16:05:28 -0300

    * use classes
    * automatic formatting

--- src/benchmark.py
+++ src/benchmark.py
@@ -13,21 +13,39 @@ def print_info(*args, **kwargs):
         print(*args, **kwargs)
 
 
-def register_backend(name,
-                     query_fn,
-                     init_fn=lambda *_: None,
-                     end_fn=lambda *_: None):
-    backend = {'query': query_fn, 'init': init_fn, 'end': end_fn}
+class Backend:
+    def __init__(self,
+                 query_fn,
+                 init_fn=lambda *_: None,
+                 end_fn=lambda *_: None):
+        self.init_fn = init_fn
+        self.query_fn = query_fn
+        self.end_fn = end_fn
+
+    def init(self, *args, **kwargs):
+        self.state = self.init_fn(*args, **kwargs)
+        return self.state
+
+    def query(self, q):
+        return self.query_fn(self.state, q)
+
+    def end(self, *args, **kwargs):
+        self.end_fn(self.state, *args, **kwargs)
+
+
+def register_backend(name, backend):
+    assert isinstance(backend, Backend)
     BACKENDS[name] = backend
     return backend
 
 
-def compare_queries(backend, backend_state, q1, q2):
-    c1 = backend['query'](backend_state, q1)
-    c2 = backend['query'](backend_state, q2)
+def compare_queries(backend, q1, q2):
+    c1 = backend.query(q1)
+    c2 = backend.query(q2)
     r1 = c1.fetchall()
     r2 = c2.fetchall()
     return r1 == r2, r1, r2
+
 
 def benchmark(backend,
               address,
@@ -36,24 +54,25 @@ def benchmark(backend,
               number=100,
               check_equivalent=False):
     # start DB connection
-    backend_state = backend['init'](address, port)
+    backend.init(address, port)
     try:
         times = {}
         for qname, (q_a, q_b) in queries.items():
             print_info("Running query {}".format(qname), file=sys.stderr)
             if check_equivalent:
-                same_query_plan, _, _ = compare_queries(backend, backend_state, "EXPLAIN QUERY PLAN " + q_a, "EXPLAIN QUERY PLAN " + q_b)
+                same_query_plan, _, _ = compare_queries(
+                    backend, "EXPLAIN QUERY PLAN " + q_a,
+                    "EXPLAIN QUERY PLAN " + q_b)
                 if same_query_plan:
-                    print(
-                        "Query plans for {} are the same".format(qname),
-                        file=sys.stderr
-                    )
+                    print("Query plans for {} are the same".format(qname),
+                          file=sys.stderr)
                 else:
-                    same_results, res_a, res_b = compare_queries(backend, backend_state, q_a, q_b)
+                    same_results, res_a, res_b = compare_queries(
+                        backend, q_a, q_b)
                     if not same_results:
-                        print(
-                            "Query results for {} are not the same".format(qname),
-                            file=sys.stderr)
+                        print("Query results for {} are not the same".format(
+                            qname),
+                              file=sys.stderr)
                         import tempfile
                         tmpdir = Path(tempfile.gettempdir())
                         for ix, res in enumerate([res_a, res_b]):
@@ -65,20 +84,21 @@ def benchmark(backend,
                                 write_csv(outfile, res)
                     else:
                         print_info(
-                            "Query results for {} are equivalent".format(qname),
+                            "Query results for {} are equivalent".format(
+                                qname),
                             file=sys.stderr)
-                        times_a = timeit.repeat(lambda: all(backend['query']
-                                                            (backend_state, q_a)),
-                                                repeat=number,
-                                                number=1)
-                        times_b = timeit.repeat(lambda: all(backend['query']
-                                                            (backend_state, q_b)),
-                                            repeat=number,
-                                                number=1)
+                        times_a = timeit.repeat(
+                            lambda: all(backend.query(q_a)),
+                            repeat=number,
+                            number=1)
+                        times_b = timeit.repeat(
+                            lambda: all(backend.query(q_b)),
+                            repeat=number,
+                            number=1)
                         times[qname] = (times_a, times_b)
     finally:
         # end DB connection
-        assert (backend['end'](backend_state))
+        assert (backend.end())
     return times
 
 
@@ -140,10 +160,7 @@ def sqlite3_end(con):
     return True
 
 
-register_backend('sqlite3',
-                 sqlite3_query,
-                 init_fn=sqlite3_init,
-                 end_fn=sqlite3_end)
+register_backend("sqlite3", Backend(sqlite3_query, sqlite3_init, sqlite3_end))
 
 if __name__ == "__main__":
     import argparse
